9d1f159f391149748f0310d2304c3f410de15288
Geoff Priestner <Geoff@DN-UK.COM>
2024-06-11 10:19:57 +0100

    Added rotation

--- index.js
+++ index.js
@@ -43,11 +43,13 @@ class Cube {
     constructor(x, y, z, s = 1) {
         this.scale = s;
         this.position = { x, y, z };
+        this.rotation = { x: 0, y: 0, z: 0 };
     }
     draw() {
         const points = [];
         for (let i = 0; i < this.model.length; ++i) {
-            const wp = this.toWorldPoint(this.model[i]);
+            const lp = this.toLocalPoint(this.model[i]);
+            const wp = this.toWorldPoint(lp);
             const cp = this.toXyPoint(wp);
             points.push(cp);
         }
@@ -76,8 +78,24 @@ class Cube {
 
         view.stroke();
     }
+    rotate(p, rotation, axis) {
+        const angle = rotation[axis];
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+        switch(axis) {
+            case "x": return { x: p.x, y: p.y * cos - p.z * sin, z: p.y * sin + p.z * cos };
+            case "y": return { x: p.x * cos - p.z * sin, y: p.y, z: p.x * sin + p.z * cos };
+            case "z": return { x: p.x * cos - p.y * sin, y: p.x * sin + p.y * cos, z: p.z };
+        }
+    }
     moveTo(p) { view.moveTo(p.x, p.y); }
     lineTo(p) { view.lineTo(p.x, p.y); }
+    toLocalPoint(p) {
+        const rx = this.rotate(p, this.rotation, "x");
+        const ry = this.rotate(rx, this.rotation, "y");
+        const rz = this.rotate(ry, this.rotation, "z");
+        return rz;
+    }
     toWorldPoint(p) {
         const wp = { x: this.position.x + p.x * this.scale,
                      y: this.position.y + p.y * this.scale,
@@ -98,6 +116,8 @@ gui.add(cube.position, "y", -30, 30);
 gui.add(cube.position, "z", 5, 250);
 function animate() {
     view.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+
+    cube.rotation.x += 0.01;
     cube.draw();
 
     requestAnimationFrame(animate);
